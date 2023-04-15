eb4fefcffd4f45c0c4bbca8fec74aa62deefc8d1
TheEvilIce <neo2_neo@mail.ru>
2023-04-16 02:07:21 +0300

    poluchilos

--- less5.1.ps1
+++ less5.1.ps1
@@ -1,4 +1,16 @@
-class Dice {
-    [int]$Max
-    [int]$Min
+#  Write a PowerShell class to implement pow(x, n) that is returns the base to the exponent power (baseexponent).
+# Example 1
+
+# $MyCustomObject.Pow(2, 3)
+
+# Returns
+
+# 8
+
+class stepen
+{
+    [double]Pow([double]$a, [double]$b)
+    {
+        return [Math]::Pow($a, $b)
+    }
 }
